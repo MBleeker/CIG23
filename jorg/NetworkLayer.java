a27e05a8d56fa918a099a7e3759152fe42171400
toologicbv <jorg.sander@toologic.com>
2015-11-11 16:19:43 +0100

    new version 11-11-2015 v1

---
+++ jorg/NetworkLayer.java
@@ -0,0 +1,146 @@
+import java.io.Serializable;
+
+import Jama.Matrix;
+
+/* Jorg: we need to make sure that we can store the whole NN as a binary object
+		 which means together with the learned parameter matrix as a whole
+		 classes IGenome and DefaultDriverGenome are also serializable
+		 and our NN will be a property of the class DefaultDriverGenome	 
+ */
+class NetworkLayer implements Serializable {
+	
+	private static final long serialVersionUID = 100000000000001L;
+	
+	private int numberOfNeurons;
+	private int layerType;
+	private Matrix weightMatrix;
+	private Matrix activationVector;
+	private Matrix outputVector;
+	
+	private NetworkLayer previousLayer;
+	private NetworkLayer nextLayer;
+	
+	/*
+	 * Constructor
+	 * 
+	 * @param numberOfNeurons 
+	 * @param layerType 1: input, 2: hidden, 3: output
+	 */
+	public NetworkLayer(int numberOfNeurons, int layerType) {
+		this.numberOfNeurons = numberOfNeurons;
+		this.layerType = layerType;
+		this.previousLayer = null;
+		this.nextLayer = null;
+		this.activationVector = new Matrix(new double[numberOfNeurons], 1); 
+		this.outputVector = new Matrix(new double[numberOfNeurons], 1);
+		
+	}
+	
+	public int getNumberOfNeurons() {
+		return numberOfNeurons;
+	}
+
+	public int getLayerType() {
+		return layerType;
+	}
+
+	public NetworkLayer getPreviousLayer() {
+		return previousLayer;
+	}
+
+	public NetworkLayer getNextLayer() {
+		return nextLayer;
+	}
+	
+	public Matrix getWeightMatrix() {
+		return weightMatrix;
+	}
+	
+	public Matrix getActivationVector() {
+		return activationVector;
+	}
+
+	public Matrix getOutputVector() {
+		return outputVector;
+	}
+
+	public void setOutputVector(Matrix outputVector) {
+		this.outputVector = outputVector;
+	}
+
+		
+	/*
+	 * Method to set the previous layer of the current layer.
+	 * You want to know the previous layer and the next layer, in order to be
+	 * able to know which layers are connected.
+	 * 
+	 * @param previousLayer The layer of nodes before the current layer
+	 */
+	public void setPreviousLayer(NetworkLayer previousLayer) {
+		this.previousLayer = previousLayer;
+	}
+	
+	/*
+	 * Method to set the next layers of the current layer.
+	 * 
+	 * @param nextLayer The layer of nodes after the current layer
+	 */
+	public void setNextLayer (NetworkLayer nextLayer) {
+		this.nextLayer = nextLayer;
+	}
+	
+	public void initializeWeightMatrix() {
+	
+		// if there is no previous layer we can't initialize
+		// e.g. the InputLayer has no WeightMatrix
+		if (this.previousLayer != null) {
+			// we are going to construct an N x M matrix for this layer
+			// M = number of units previous layer
+			// M = number of units this layer
+			// so we go from M-dim to N-dim 
+			int M = this.getPreviousLayer().getNumberOfNeurons();
+			int N = this.getNumberOfNeurons();
+			this.weightMatrix = Matrix.random(N, M);
+			
+		}
+	}
+	
+	public void calculateActivation(Matrix input) {
+		
+		System.out.println("Weight dim " + this.getDimMatrix(this.getWeightMatrix()) );
+		System.out.println("Input dim " + this.getDimMatrix(input) );
+		this.activationVector = this.getWeightMatrix().times(input);
+		System.out.println("Output dim " + this.getDimMatrix(this.activationVector) );
+	}
+	
+	public void calculateOutput() {
+		
+		double[][] hx = this.activationVector.getArray();
+	    int n = this.activationVector.getRowDimension();
+	    int m = this.activationVector.getColumnDimension();
+
+	    for (int i = 0; i < n; i++) {
+	        for (int j = 0; j<m; j++) {
+	             hx[i][j] = this.Tanh(hx[i][j]);
+	        }
+	    }
+	    this.outputVector = new Matrix(hx);
+	}
+	
+	protected double SigmoidFunction(double num){
+		
+		return 1.0 / (1.0 + Math.exp(-1.0 * num));
+	}
+	
+	protected double Tanh(double num) {
+		final double result = (Math.exp(num*2.0)-1.0)/(Math.exp(num*2.0)+1.0);
+		return result;
+	}
+	
+	private String getDimMatrix(Matrix a) {
+		
+		int M = a.getColumnDimension();
+		int N = a.getRowDimension();
+		return "(" + N + ", " + M + ")";
+	}
+}
---
+++ jorg/NeuralNetwork.java
@@ -0,0 +1,142 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import Jama.Matrix;
+
+/*
+ * Class to implement the feed forward neural network that is 
+ * used by the bot to drive the track. The network consists
+ * of an input layer, followed by (a number of) hidden layer(s)
+ * and one output node. The network is trained using
+ * backpropagation. 
+ */
+public class NeuralNetwork implements Serializable {
+	
+	private static final long serialVersionUID = 100000000000001L;
+	
+	public static class WrongBuildSequence extends Exception implements Serializable {
+		
+		private static final long serialVersionUID = 200000000000001L;
+	}
+	
+	/*
+	 * Constructor
+	 */
+	public NeuralNetwork(){
+		
+	}
+	
+	NetworkLayer inputLayer = null;
+	NetworkLayer outputLayer = null;
+	
+	protected List<NetworkLayer> allLayers = new ArrayList<NetworkLayer>();
+	
+	/*
+	 * Method to construct an input layer
+	 * @param numberOfNeurons the number of Neurons the layer contains
+	 */
+	public void buildInputLayer(int numberOfNeurons) {		
+		NetworkLayer newInputLayer = new NetworkLayer(numberOfNeurons, 1);
+		
+		this.inputLayer = newInputLayer;	
+		this.allLayers.add(this.inputLayer);
+		
+	}
+	
+	/*
+	 * Method to construct a hidden layer
+	 * @param numberOfNeurons the number of Neurons the layer contains
+	 */
+	public void buildHiddenLayer(int numberOfNeurons) throws WrongBuildSequence {
+		NetworkLayer newHiddenLayer = new NetworkLayer(numberOfNeurons, 2);
+		
+		/* Assume that every new hidden layer is positioned after an already
+		 * existing one (except for the first one) */
+		
+		if (this.allLayers.isEmpty()) {
+			// throw exception because you first have to construct the 
+			// input layer
+			throw new WrongBuildSequence();
+		}
+		else {
+			newHiddenLayer.setPreviousLayer(this.allLayers.get(this.allLayers.size()-1) );
+		}
+		
+		// first initialize the weights
+		// then set the forward pointer in the previous layer
+		newHiddenLayer.initializeWeightMatrix();
+		this.setNextLayerInPreviousLayer(newHiddenLayer);
+		this.allLayers.add(newHiddenLayer);
+	}
+	
+
+	/*
+	 * Method to construct an output layer
+	 * @param numberOfNeurons the number of Neurons the layer contains
+	 */
+	public void buildOutputLayer(int numberOfNeurons) throws WrongBuildSequence {
+		NetworkLayer newOutputLayer = new NetworkLayer(numberOfNeurons, 3);
+		
+		if (this.allLayers.isEmpty()) {
+			// throw exception because you first have to construct the 
+			// input layer
+			throw new WrongBuildSequence();
+		}
+		else {
+			newOutputLayer.setPreviousLayer(this.allLayers.get(this.allLayers.size()-1) );
+		}
+		
+		// first initialize the weights
+		// then set the forward pointer in the previous layer
+		newOutputLayer.initializeWeightMatrix();
+		this.outputLayer = newOutputLayer; 
+		this.allLayers.add(this.outputLayer);
+	}
+	
+	private void setNextLayerInPreviousLayer(NetworkLayer nl){
+		
+		if (!this.allLayers.isEmpty()) {
+			this.allLayers.get(this.allLayers.size()-1).setNextLayer(nl);
+		}
+		else
+		{ // must be first layer that we add therefore we can't assign
+		  // the "nextLayer" to the previous layer
+		}
+	}
+	
+	public List<NetworkLayer> getAllLayers() {
+		return allLayers;
+	}
+
+	public void processInput(Matrix inputVec) {
+		
+		// set output vector for input layer because we're not going
+		// to calculate the output for that layer
+		this.inputLayer.setOutputVector(inputVec);
+		
+		for (NetworkLayer aLayer : this.allLayers) {
+			if (aLayer.getWeightMatrix() != null) {
+				// get output of previous layer
+				Matrix outputPrevLayer = this.allLayers.get(this.allLayers.indexOf(aLayer)-1).getOutputVector();
+				aLayer.calculateActivation(outputPrevLayer);
+				aLayer.calculateOutput();
+				System.out.println("Layer " + aLayer.getNumberOfNeurons());
+				System.out.println(Arrays.deepToString(aLayer.getActivationVector().getArray()));
+			}
+		}
+	}
+}
+
+
+/*public class NeuralNetwork {
+
+	NeuralNetwork(){ }
+
+	NeuralNetwork(int inputs, int hidden, int outputs){}
+
+	public double getOutput(SensorModel a) {
+		return 0.5;
+	}
+}*/
