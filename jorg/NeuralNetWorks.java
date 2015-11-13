1728ef212c38ae5315dd8bade33c050d89dd7eaf
toologicbv <jorg.sander@toologic.com>
2015-11-13 17:23:40 +0100

    endlessly pushing this file

--- jorg/NeuralNetWorks.java
+++ jorg/NeuralNetWorks.java
@@ -11,9 +11,9 @@ public class NeuralNetWorks {
 		NeuralNetwork MyNN = new NeuralNetwork();
 		
 		try {
-			MyNN.buildInputLayer(6);
-			MyNN.buildHiddenLayer(4);
-			MyNN.buildOutputLayer(1);
+			MyNN.buildInputLayer(6, "sig");
+			MyNN.buildHiddenLayer(4, "sig");
+			MyNN.buildOutputLayer(2, "sig");
 		}
 		catch (NeuralNetwork.WrongBuildSequence e){
 			e.printStackTrace();
@@ -21,18 +21,23 @@ public class NeuralNetWorks {
 		
 		List<NetworkLayer> allLayers = MyNN.getAllLayers();
 		for (NetworkLayer alayer : allLayers) {
-			System.out.println("Print layer of type " + alayer.getLayerType());
+			// System.out.println("Print layer of type " + alayer.getLayerType());
 			if (alayer.getWeightMatrix() != null) {
-				System.out.println(Arrays.deepToString(alayer.getWeightMatrix().getArray()));
-				System.out.println(Arrays.deepToString(alayer.getOutputVector().getArray()));
+				// System.out.println(Arrays.deepToString(alayer.getWeightMatrix().getArray()));
+				// System.out.println(Arrays.deepToString(alayer.getOutputVector().getArray()));
 
 			}
 		}
 		
 		Matrix o = new Matrix(new double[][] {{2.0}, {2.0}, {2.0}});
-		System.out.println(o.getRowDimension() + ", " + o.getColumnDimension());
+		// System.out.println(o.getRowDimension() + ", " + o.getColumnDimension());
 		Matrix input = new Matrix(new double[][] {{2.0}, {2.0}, {2.0}, {2.0}, {3.0}, {4.0}}); 
 		MyNN.processInput(input);
+		
+		Matrix target = new Matrix(new double[][] {{2.0}, {2.0}});
+		BackProp BP = new BackProp(MyNN, target, 0.001);
+		BP.computeBackProp();
+		
 	}
 
 }
